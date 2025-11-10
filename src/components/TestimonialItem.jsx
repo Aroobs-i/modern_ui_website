606f6af8eaa27a82af0d9b48bfdf5a9579179afa
Aroobs-i <iftikhar8995@gmail.com>
2025-11-10 16:40:02 +0500

    Testimonials  section

---
+++ src/components/TestimonialItem.jsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx'
+import React from 'react'
+
+const TestimonialItem = ({ item, containerClass }) => {
+  return (
+    <div className={clsx('relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0')}>
+
+    </div>
+
+  )
+}
+
+export default TestimonialItem
